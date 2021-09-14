dd332d5ec6903d57edc13a11f6b5cb63072e9fc1
MeliMarquez <melanie.marquez@ug.uchile.cl>
2021-09-14 01:19:23 -0400

    Modulo de videos: capturar acciones usuario

--- Client/src/app/components/video/video.component.ts
+++ Client/src/app/components/video/video.component.ts
@@ -22,22 +22,49 @@ export class VideoComponent implements OnInit {
 
 
   played(event: Plyr.PlyrEvent) {
-    console.log('played', event);
+    if (event.type === 'play'){
+      console.log('play on ', this.player.currentTime);
+    }
+    else if (event.type === 'pause'){
+      console.log('pause on ', this.player.currentTime);
+    }
+    else if (event.type === 'ratechange'){
+      console.log('cambio de speed to ', this.player.speed);
+    }
+    else if (event.type === 'enterfullscreen'){
+      console.log('pantalla completa on ', this.player.currentTime);
+    }
+    else if (event.type === 'exitfullscreen'){
+      console.log('salir de pantalla completa on ', this.player.currentTime);
+    }
+    else if (event.type === 'ended') {
+      console.log('video terminado');
+    }
+    else if (event.type === 'volumechange'){
+      console.log('volumen cambio a ', this.player.volume, ' on time ', this.player.currentTime);
+    }
+    else if (event.type === 'seeking'){ // Estos no funcionan bien bien
+      console.log('mover desde ', this.player.currentTime);
+    }
+    else if (event.type === 'seeked'){
+      console.log('y dejar on ', this.player.currentTime);
+    }
   }
 
 
   play(): void {
+    this.player.pip = false; // Hemos sido timados
     this.player.play();
   }
 
 
   ngOnInit(): void {
     this.getVideo();
-    this.poster = "/assets/videoModule-images/banner-video" + this.getVideo() + ".jpg";
+    this.poster = '/assets/videoModule-images/banner-video' + this.getVideo() + '.jpg';
     this.videoSources = [
       {
         src: '/assets/videoModule-videos/video' + this.getVideo() + '.mp4',
-        type: "video/mp4"
+        type: 'video/mp4'
       }
     ];
   }
@@ -52,4 +79,7 @@ export class VideoComponent implements OnInit {
     this.newItemEvent.emit(value);
   }
 
+  pause() {
+    console.log('pause');
+  }
 }
