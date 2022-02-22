bf835ca4369be4097e565a1f94d7908fa5cf3291
bellyster <bellysterplus@gmail.com>
2022-02-22 19:57:18 -0300

    quiz maintainer

--- Client/src/app/components/quiz-mantainer/quiz-mantainer.component.ts
+++ Client/src/app/components/quiz-mantainer/quiz-mantainer.component.ts
@@ -4,27 +4,16 @@ import { TranslateService } from '@ngx-translate/core';
 import { trigger, transition, animate, style } from '@angular/animations'
 
 @Component({
-  animations: [
-    trigger('slideInOut', [
-      transition(':enter', [
-        style({transform: 'translateY(-100%)'}),
-        animate('200ms ease-in', style({transform: 'translateY(0%)'}))
-      ]),
-      transition(':leave', [
-        animate('200ms ease-in', style({transform: 'translateY(-100%)'}))
-      ])
-    ])
-  ],
   selector: 'app-quiz-mantainer-component',
   templateUrl: './quiz-mantainer.component.html',
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['./quiz-mantainer.component.css']
 })
 export class QuizMantainerComponent {
-
- visible= true;
- toggleDiv(){
-   this.visible= !this.visible;
+ verQuizes=true;
+ crearQuizToogle(){
+    this.verQuizes= !this.verQuizes;
  }
+ 
 
 }
