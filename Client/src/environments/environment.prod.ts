a11605602fa61f076468b485e8f58bc98fa437d3
bellyster <bellysterplus@gmail.com>
2022-03-31 12:30:08 -0300

    commit nuevo para video module

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,7 +1,12 @@
 export const environment = {
   production: true,
   serverRoot: 'https://trainer.neurone.info/',
+<<<<<<< Updated upstream
   apiURL: 'https://trainer.neurone.info/api/',
   frontURL: 'https://trainer.neurone.info/',
+=======
+   apiURL: 'https://trainer.neurone.info/api/',
+   frontURL: 'https://trainer.neurone.info/',
+>>>>>>> Stashed changes
   locale: 'es-CL'
 };
