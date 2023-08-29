d024cb363621d45295140973c6ac557d35f55a69
bellyster <bellysterplus@gmail.com>
2023-08-28 23:09:16 -0400

    otra vez link youtube

--- Client/src/app/components/video-mantainer/video-mantainer.component.ts
+++ Client/src/app/components/video-mantainer/video-mantainer.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectorRef, Component, OnInit, ViewEncapsulation  } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { TranslateService } from '@ngx-translate/core';
 import { QuizService } from '../../services/videoModule/quiz.service';
@@ -10,151 +15,158 @@ import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
-
 @Component({
   selector: 'app-video-mantainer-component',
 
   templateUrl: './video-mantainer.component.html',
   encapsulation: ViewEncapsulation.None,
-  styleUrls: ['./video-mantainer.component.css']
+  styleUrls: ['./video-mantainer.component.css'],
 })
+export class VideoMantainerComponent implements OnInit {
+  videos = [];
+  constructor(
+    private quizService: QuizService,
+    public matDialog: MatDialog,
+    private http: HttpClient,
+    private toastr: ToastrService,
+    private translate: TranslateService,
+    private router: Router
+  ) {}
 
-export class VideoMantainerComponent implements OnInit{
-  
-  videos=[];
-  constructor(private quizService: QuizService,
-              public matDialog: MatDialog,
-              private http: HttpClient,
-              private toastr: ToastrService,
-              private translate: TranslateService,
-              private router: Router) {  }
+  ngOnInit(): void {
+    this.loadVideos();
+  }
 
- ngOnInit(): void{
-  this.loadVideos();
- }
-
- loadVideos(){
-  this.quizService.getVideos().subscribe(
-    (res)=>{
-      this.videos=[];
-      for (let i in res.data){
+  loadVideos() {
+    this.quizService.getVideos().subscribe((res) => {
+      this.videos = [];
+      for (let i in res.data) {
         this.videos.push({
           name: res.data[i].name,
           language: res.data[i].language,
-          _id: res.data[i]._id, 
+          _id: res.data[i]._id,
           video_url: res.data[i].video_url,
           image_url: res.data[i].image_url,
-        })
-        console.log(this.videos)
+        });
+        console.log(this.videos);
       }
       this.videos.reverse();
-    })
- }
-  
-
-
-loadImage(file){
-  let formData = new FormData();
-  formData.append('file', file);
-  this.quizService.saveImage(formData).subscribe(
-    (res)=>{
-      console.log(res)
-    }
-  )
-  return;
-}
-languageVideo="es";
-error={
-  name:false,
-  url:false,
-  file:false
-};
-validate(name, url, file, language){
-  console.log(name, url, file, language);
-  this.error.name=false; 
-  this.error.url= false;
-  this.error.file=false;
-  if (!file){
-    this.error.file=true;
+    });
   }
-  if (name.length==0) this.error.name=true;
-  if(url.length<10) this.error.url=true;
-  if (this.error.name || this.error.url || this.error.file){
-    return false
-  }
-  return true
-}
-cleanForm(){
-  (document.getElementById("tituloVideo")  as HTMLInputElement).value="";
-  (document.getElementById("imageVideo")  as HTMLInputElement).value=null;
 
-  //limpiar campos de video falta. 
-  (document.getElementById("video")  as HTMLInputElement).value=null;
-  this.url=null;
-}
-registerVideo(){
-  let name= (document.getElementById("tituloVideo")  as HTMLInputElement).value;
-  let url_video= this.url;
-  let file = (document.getElementById("imageVideo")  as HTMLInputElement).files[0];
-  let language= this.languageVideo;
-  let formData = new FormData();
-
-  if (this.validate(name, url_video, file, language)){
+  loadImage(file) {
+    let formData = new FormData();
     formData.append('file', file);
-    this.quizService.saveImage(formData).subscribe(
-     (res:any)=>{
-       let image= res.url;
-        let objetVideo={
-          name: name, 
-          video_url:url_video,
-          image_url: image, 
-          language:language
-        }
-        this.quizService.addVideo(objetVideo).subscribe(
-          (res)=>{
+    this.quizService.saveImage(formData).subscribe((res) => {
+      console.log(res);
+    });
+    return;
+  }
+  languageVideo = 'es';
+  error = {
+    name: false,
+    url: false,
+    file: false,
+  };
+  validate(name, url, file, language) {
+    console.log(name, url, file, language);
+    this.error.name = false;
+    this.error.url = false;
+    this.error.file = false;
+    if (!file) {
+      this.error.file = true;
+    }
+    if (name.length == 0) this.error.name = true;
+    if (url.length < 10) this.error.url = true;
+    if (this.error.name || this.error.url || this.error.file) {
+      return false;
+    }
+    return true;
+  }
+  cleanForm() {
+    (document.getElementById('tituloVideo') as HTMLInputElement).value = '';
+    (document.getElementById('imageVideo') as HTMLInputElement).value = null;
+
+    //limpiar campos de video falta.
+    (document.getElementById('video') as HTMLInputElement).value = null;
+    this.url = null;
+  }
+  registerVideo() {
+    let name = (document.getElementById('tituloVideo') as HTMLInputElement)
+      .value;
+    let urlYoutube = (document.getElementById('urlVideo') as HTMLInputElement)
+      .value;
+    let url_video = '';
+    let file = (document.getElementById('imageVideo') as HTMLInputElement)
+      .files[0];
+    let language = this.languageVideo;
+    let formData = new FormData();
+    if (urlYoutube && urlYoutube != '') {
+      url_video = urlYoutube;
+      formData.append('file', file);
+      this.quizService.saveImage(formData).subscribe((res: any) => {
+        let image = res.url;
+        let objetVideo = {
+          name: name,
+          video_url: url_video,
+          image_url: image,
+          language: language,
+        };
+        this.quizService.addVideo(objetVideo).subscribe((res) => {
+          console.log(res);
+          this.loadVideos();
+          this.cleanForm();
+        });
+      });
+    } else {
+      url_video = this.url;
+      if (this.validate(name, url_video, file, language)) {
+        formData.append('file', file);
+        this.quizService.saveImage(formData).subscribe((res: any) => {
+          let image = res.url;
+          let objetVideo = {
+            name: name,
+            video_url: url_video,
+            image_url: image,
+            language: language,
+          };
+          this.quizService.addVideo(objetVideo).subscribe((res) => {
             console.log(res);
             this.loadVideos();
             this.cleanForm();
-          }
-        )
-     }
-   )
-  }
-   
-}
-deleteVideo(videoId){
-  this.quizService.deleteVideo(videoId).subscribe(
-    (res)=>{
-      this.loadVideos();
+          });
+        });
+      }
     }
-  )
-}
+  }
+  deleteVideo(videoId) {
+    this.quizService.deleteVideo(videoId).subscribe((res) => {
+      this.loadVideos();
+    });
+  }
 
-//VIDEO UPLOADER
-url;
-format;
-loadingVideo= false;
-onSelectFile(event) {
+  //VIDEO UPLOADER
+  url;
+  format;
+  loadingVideo = false;
+  onSelectFile(event) {
     const file = event.target.files && event.target.files[0];
     if (file) {
       var reader = new FileReader();
       reader.readAsDataURL(file);
       //verificar que el tipo sea video.
-      if(file.type.indexOf('video')> -1){
-        this.loadingVideo= true;
+      if (file.type.indexOf('video') > -1) {
+        this.loadingVideo = true;
         this.format = 'video';
 
-        //Guardar el video 
+        //Guardar el video
         let formData = new FormData();
         formData.append('file', file);
-        this.quizService.saveImage(formData).subscribe(
-          (res:any)=>{
-            console.log(res)
-            this.url= res.url;
-            this.loadingVideo=false;
-          }
-        )
-
+        this.quizService.saveImage(formData).subscribe((res: any) => {
+          console.log(res);
+          this.url = res.url;
+          this.loadingVideo = false;
+        });
       }
     }
   }
--- Client/src/app/components/video/video.component.ts
+++ Client/src/app/components/video/video.component.ts
@@ -1,17 +1,23 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { PlyrComponent, PlyrModule } from 'ngx-plyr';
 import { QuizService } from '../../services/videoModule/quiz.service';
 import { StageService } from '../../services/trainer/stage.service';
-
 
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
   encapsulation: ViewEncapsulation.Emulated, // ???
-  styleUrls: ['./video.component.css']
+  styleUrls: ['./video.component.css'],
 })
 export class VideoComponent implements OnInit {
-
   @Input() videoNumber: number;
   @Output() newItemEvent = new EventEmitter<string>();
 
@@ -33,47 +39,59 @@ export class VideoComponent implements OnInit {
   flowId;
   loadingVideo = true;
 
-  constructor( private plyrModule: PlyrModule,
-               private quizService: QuizService,
-               private stageService: StageService) { }
-
-
+  constructor(
+    private plyrModule: PlyrModule,
+    private quizService: QuizService,
+    private stageService: StageService
+  ) {}
 
   played(event: Plyr.PlyrEvent) {
     let eventFinal;
-    if (event.type === 'play'){
+    if (event.type === 'play') {
       eventFinal = 'Play en ' + this.player.currentTime + ' s.';
-    }
-    else if (event.type === 'pause'){
+    } else if (event.type === 'pause') {
       eventFinal = 'Pause en ' + this.player.currentTime + ' s.';
-    }
-    else if (event.type === 'ratechange'){
-      eventFinal = 'Cambio de velocidad a ' + this.player.speed + ' en ' + this.player.currentTime + ' s.';
-    }
-    else if (event.type === 'enterfullscreen'){
+    } else if (event.type === 'ratechange') {
+      eventFinal =
+        'Cambio de velocidad a ' +
+        this.player.speed +
+        ' en ' +
+        this.player.currentTime +
+        ' s.';
+    } else if (event.type === 'enterfullscreen') {
       eventFinal = 'Pantalla completa en ' + this.player.currentTime + ' s.';
-    }
-    else if (event.type === 'exitfullscreen'){
-      eventFinal = 'Salir de pantalla completa en ' + this.player.currentTime + ' s.';
-    }
-    /*else if (event.type === 'ended') {
+    } else if (event.type === 'exitfullscreen') {
+      eventFinal =
+        'Salir de pantalla completa en ' + this.player.currentTime + ' s.';
+    } else if (event.type === 'volumechange') {
+      /*else if (event.type === 'ended') {
       eventFinal = 'Video terminado. Tiempo total: ';
     }*/
-    else if (event.type === 'volumechange'){
-      eventFinal = 'Volumen cambio a ' + this.player.volume + ' en ' + this.player.currentTime + ' s.';
-    }
-    else if (event.type === 'seeked'){
-      if(this.player.currentTime != this.currentTime){
+      eventFinal =
+        'Volumen cambio a ' +
+        this.player.volume +
+        ' en ' +
+        this.player.currentTime +
+        ' s.';
+    } else if (event.type === 'seeked') {
+      if (this.player.currentTime != this.currentTime) {
         let valor = this.player.currentTime;
         valor = parseFloat(valor.toFixed(2));
         eventFinal = 'Cambiar tiempo a ' + valor;
       }
     }
-    if (eventFinal != null && this.saveUserData === 'Yes'){
-      this.quizService.handleEvent(eventFinal, 'video', this.userId, this.stageId, this.flowId).subscribe((res) => { });
+    if (eventFinal != null && this.saveUserData === 'Yes') {
+      this.quizService
+        .handleEvent(
+          eventFinal,
+          'video',
+          this.userId,
+          this.stageId,
+          this.flowId
+        )
+        .subscribe((res) => {});
     }
   }
-
 
   play(): void {
     this.player.play();
@@ -81,33 +99,47 @@ export class VideoComponent implements OnInit {
     this.player.speed = 1;
   }
 
-
   ngOnInit(): void {
     // Get the video
     console.log('Buscamos... ');
     console.log(this.videoNumber);
-    this.quizService.getVideo(this.videoNumber).subscribe(res => {
-      console.log(res)
-      this.videoJson = res["data"];
+    this.quizService.getVideo(this.videoNumber).subscribe((res) => {
+      console.log(res);
+      this.videoJson = res['data'];
       this.poster = this.videoJson.image_url;
-      this.videoSources = [
-        {
-          src: this.videoJson.video_url,
-          type: 'video/mp4'
-        }
-      ];
-      this.loadingVideo=false;
+      let ytSrc = this.checkYoutubeSrc(this.videoJson.video_url);
+      if (ytSrc) {
+        this.videoSources = [
+          {
+            src: this.videoJson.video_url,
+            provider: 'youtube',
+          },
+        ];
+      } else {
+        this.videoSources = [
+          {
+            src: this.videoJson.video_url,
+            type: 'video/mp4',
+          },
+        ];
       }
-    );
+      this.loadingVideo = false;
+    });
 
-    this.stageId = localStorage.getItem('stageId', );
-    this.userId = JSON.parse(localStorage.getItem('currentUser', ))._id;
-    this.stageService.getStage(this.stageId).subscribe(res => {
+    this.stageId = localStorage.getItem('stageId');
+    this.userId = JSON.parse(localStorage.getItem('currentUser'))._id;
+    this.stageService.getStage(this.stageId).subscribe((res) => {
       this.flowId = res['stage'].flow;
     });
   }
 
-/*
+  checkYoutubeSrc(url) {
+    let youtubeSrc = url.includes('youtube');
+    console.log('IS THIS A YOUTUBE SRC?');
+    console.log(youtubeSrc);
+    return youtubeSrc;
+  }
+  /*
   sendVideoResponse(value) {
     this.newItemEvent.emit(value);
   }
